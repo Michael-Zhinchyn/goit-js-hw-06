3230d1c6a0fe0be5cba7efb81e55ab71822fc58d
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-05-20 23:24:26 -0500

    update

---
+++ js/task-06.js
@@ -0,0 +1,19 @@
+const input = document.querySelector('#validation-input');
+
+input.addEventListener('blur', function(event) {
+  const inputValue = event.target.value;
+  const requiredLength = parseInt(input.dataset.length);
+
+  if (inputValue.length === requiredLength) {
+  
+    input.classList.add('valid');
+    input.classList.remove('invalid');
+  } else {
+    input.classList.add('invalid');
+    input.classList.remove('valid');
+  }
+});
+
+
+
+

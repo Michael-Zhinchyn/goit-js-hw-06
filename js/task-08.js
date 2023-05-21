c02154845bda13f7a928eb8f3681831f110812ae
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-05-21 17:56:45 -0500

    task-8 done. Update

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const form = document.querySelector('.login-form')
+
+form.addEventListener('submit', onSubmit)
+
+function onSubmit(event) {
+  event.preventDefault()
+
+    const {email, password} = event.currentTarget.elements
+
+    if (email === "" || password === "") {
+      
+      alert('Будь ласка, заповніть всі поля')
+      
+  } else {
+   
+    const userData = {
+      email: email.value,
+      password: password.value,
+    }
+
+    console.dir(userData)
+
+    event.target.reset()
+  }
+}
 
